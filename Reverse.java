714e9939ba7f3cd2d9f5fc416e62fea74d79c145
RMonisha-7 <monigopal95@gmail.com>
2016-07-19 11:08:07 +0530

    Add files via upload
    ds

---
+++ Reverse.java
@@ -0,0 +1,14 @@
+package mm;
+
+public class Reverse {
+
+	
+	public static void main(String[] args) {
+		StringBuffer s=new StringBuffer("priya");
+		System.out.println(s);
+		s.reverse();
+		System.out.println(s);
+
+	}
+
+}
