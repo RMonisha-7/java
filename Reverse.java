a9ccb2df8da3f6cd1f3bd3793086c4a2ea1e4e81
RMonisha-7 <monigopal95@gmail.com>
2016-07-24 11:59:11 +0530

    Add files via upload
    gh

--- Reverse.java
+++ Reverse.java
@@ -1,14 +1,21 @@
-package mm;
+package kk;
 
 public class Reverse {
 
-	
 	public static void main(String[] args) {
-		StringBuffer s=new StringBuffer("priya");
-		System.out.println(s);
-		s.reverse();
-		System.out.println(s);
-
+		String str="hello world";
+		int c1=0;
+		String b[]=str.split(" ");
+		int c=b.length;
+		for(int i=0;i<c;i++){
+			char a[]=b[i].toCharArray();
+			c1=a.length;
+			for(int j=c1-1;j>=0;j--){
+				System.out.println(a[j]);
+	
+			}
+			
+		}
 	}
 
 }
