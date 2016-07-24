5640b399315236ccedcb51a51e9630fa6bf44e3e
RMonisha-7 <monigopal95@gmail.com>
2016-07-24 12:21:17 +0530

    Add files via upload
    rr

---
+++ Factorial.java
@@ -0,0 +1,20 @@
+package kk;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+	int	n=s.nextInt();
+	int i;
+	long fact=1;
+	for(i=1;i<=n;i++){
+		fact=fact*i;
+	}
+	System.out.println(fact);
+
+	}
+
+}
