fff1ad41a6c0bae36b16aaf0727eec25e139eba2
RMonisha-7 <monigopal95@gmail.com>
2016-08-08 14:37:42 +0530

    Add files via upload
    kk

---
+++ PrintNo.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class PrintNo {
+
+	public static void main(String[] args) {
+		int arr[]=new int []{1,2,3,4,5};
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
